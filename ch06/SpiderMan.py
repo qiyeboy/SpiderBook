38f6bbd243aa0eb294a46b1bb6937dc9df57ca0e
qiyeboy <594675898@qq.com>
2017-04-19 23:15:27 +0800

    4-11章

---
+++ ch06/SpiderMan.py
@@ -0,0 +1,41 @@
+#coding:utf-8
+from ch06.URLManager import UrlManager
+
+from ch06.HtmlDownloader import HtmlDownloader
+
+from ch06.HtmlParser import HtmlParser
+
+from ch06.DataOutput import DataOutput
+
+
+class SpiderMan(object):
+    def __init__(self):
+        self.manager = UrlManager()
+        self.downloader = HtmlDownloader()
+        self.parser = HtmlParser()
+        self.output = DataOutput()
+    def crawl(self,root_url):
+        #添加入口URL
+        self.manager.add_new_url(root_url)
+        #判断url管理器中是否有新的url，同时判断抓取了多少个url
+        while(self.manager.has_new_url() and self.manager.old_url_size()<100):
+            try:
+                #从URL管理器获取新的url
+                new_url = self.manager.get_new_url()
+                #HTML下载器下载网页
+                html = self.downloader.download(new_url)
+                #HTML解析器抽取网页数据
+                new_urls,data = self.parser.parser(new_url,html)
+                #将抽取到url添加到URL管理器中
+                self.manager.add_new_urls(new_urls)
+                #数据存储器储存文件
+                self.output.store_data(data)
+                print "已经抓取%s个链接"%self.manager.old_url_size()
+            except Exception,e:
+                print "crawl failed"
+            #数据存储器将文件输出成指定格式
+        self.output.output_html()
+
+if __name__=="__main__":
+    spider_man = SpiderMan()
+    spider_man.crawl("http://baike.baidu.com/view/284853.htm")
